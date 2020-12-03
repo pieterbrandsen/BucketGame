4d7e6b3394f93b292b0f79f19b8b7bd1bc8abe4d
Pieter <pieterbrandsen2@gmail.com>
2020-12-03 15:56:57 +0100

    Added constants for Container

---
+++ BucketGame.Constants/Container.cs
@@ -0,0 +1,20 @@
+﻿namespace BucketGame.Constants
+{
+    public static class Bucket
+    {
+        public const int BucketMinCap = 10;
+        public const int BucketDefaultCap = 12;
+        public const int BucketMaxCap = 15;
+    }
+    public static class RainBarrel
+    {
+        public const int RainBarrelSmall = 80;
+        public const int RainBarrelMedium = 120;
+        public const int RainBarrelLarge = 160;
+    }
+    public static class OilBarrel
+    {
+        public const int OilBarrelCap = 159;
+    }
+
+}
