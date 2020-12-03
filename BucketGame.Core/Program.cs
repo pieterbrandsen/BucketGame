5d0e0679769d45c883a6652a3d5c400fc68877b2
Pieter <pieterbrandsen2@gmail.com>
2020-12-03 10:39:57 +0100

    Add project files.

---
+++ BucketGame.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BucketGame.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BucketGame.Models/Bucket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucketGame.Models
+{
+    public abstract class Bucket
+    {
+        #region Constructors
+        Bucket()
+        {
+
+        }
+        Bucket(int content)
+        {
+
+        }
+        Bucket(int content, int capacity)
+        {
+
+        }
+        #endregion
+    }
+}
